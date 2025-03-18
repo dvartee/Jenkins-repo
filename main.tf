2cfa123beeebbc162268e602a01e82f92e479e8c
dvartee <71815165+dvartee@users.noreply.github.com>
2025-03-18 11:15:49 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "eu-west-2" 
 }
 resource "aws_s3_bucket" "examplebucket" {
-  bucket = "sample-bucket-2025abc"
+  bucket = "sample-bucket-2025tosin"
 }
 resource "aws_s3_bucket_versioning" "example" {
   bucket = aws_s3_bucket.examplebucket.id
