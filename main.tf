b7c175563f48af76691f1f41d41b914de6cd08a2
Oluwatosin Muraina <tosin.muraina@gmail.com>
2025-03-18 02:22:33 +0000

    Your commit message here

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "eu-west-2" 
+}
+resource "aws_s3_bucket" "examplebucket" {
+  bucket = "sample-bucket-2025abc"
+}
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.examplebucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "example_bucket_encryption" {
+  bucket = aws_s3_bucket.examplebucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
